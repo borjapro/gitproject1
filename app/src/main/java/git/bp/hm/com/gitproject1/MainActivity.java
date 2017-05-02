ea59a8919377832a4e39936c82eafd298a445af7
borja pro fernandez <borja pro fernandez>
2017-05-02 21:39:33 +0200

    Initial commit

---
+++ app/src/main/java/git/bp/hm/com/gitproject1/MainActivity.java
@@ -0,0 +1,13 @@
+package git.bp.hm.com.gitproject1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
